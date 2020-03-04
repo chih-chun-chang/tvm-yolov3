e7257d94a9a4ebded1eefd5ce288a547b6b66dbc
gary <gary30404@gmail.com>
2020-03-04 12:17:12 +0800

    update

--- build/CMakeFiles/cpptest.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cpptest"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cpptest.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
